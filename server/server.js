63dc2c64a7a67992682345728bbfd9703b85734b
LucassNogueira <simplylucasjason@gmail.com>
2022-02-09 14:03:14 -0600

    added the app.get

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 const port = process.env.PORT || 4004;
 
 app.listen(port, () => console.log("listening on port " + port));
