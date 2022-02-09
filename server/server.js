926d32d430d62c1aeaacc24952bab0066861aa5c
LucassNogueira <simplylucasjason@gmail.com>
2022-02-09 14:08:38 -0600

    set up everything

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
