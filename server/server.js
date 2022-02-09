5f7db3d73a5197abb17bd661bd21b942683a0ac6
LucassNogueira <simplylucasjason@gmail.com>
2022-02-09 14:23:47 -0600

    testing a fix

--- server/server.js
+++ server/server.js
@@ -8,8 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+app.use("/js", express.static(path.join(__dirname, "public/main.js")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/main.js"));
 });
 
 const port = process.env.PORT || 4004;
