3ee9a602a8585737a6eed9f70a8513bbda6c4062
LucassNogueira <simplylucasjason@gmail.com>
2022-02-09 16:00:06 -0600

    guess i cant use image folder

--- public/main.js
+++ public/main.js
@@ -3,51 +3,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardArray = [
     {
       name: "fries",
-      img: "../images/fries.png",
+      img: "/fries.png",
     },
     {
       name: "cheeseburger",
-      img: "../images/cheeseburger.png",
+      img: "/cheeseburger.png",
     },
     {
       name: "ice-cream",
-      img: "../images/ice-cream.png",
+      img: "/ice-cream.png",
     },
     {
       name: "pizza",
-      img: "..//pizza.png",
+      img: "/pizza.png",
     },
     {
       name: "milkshake",
-      img: "../images/milkshake.png",
+      img: "/milkshake.png",
     },
     {
       name: "hotdog",
-      img: "../images/hotdog.png",
+      img: "/hotdog.png",
     },
     {
       name: "fries",
-      img: "../images/fries.png",
+      img: "/fries.png",
     },
     {
       name: "cheeseburger",
-      img: "../images/cheeseburger.png",
+      img: "/cheeseburger.png",
     },
     {
       name: "ice-cream",
-      img: "../images/ice-cream.png",
+      img: "/ice-cream.png",
     },
     {
       name: "pizza",
-      img: "../images/pizza.png",
+      img: "/pizza.png",
     },
     {
       name: "milkshake",
-      img: "../images/milkshake.png",
+      img: "/milkshake.png",
     },
     {
       name: "hotdog",
-      img: "../images/hotdog.png",
+      img: "/hotdog.png",
     },
   ];
 
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement("img");
-      card.setAttribute("src", "../images/blank.png");
+      card.setAttribute("src", "/blank.png");
       card.setAttribute("data-id", i);
       card.addEventListener("click", flipCard);
       grid.appendChild(card);
@@ -77,19 +77,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const optionTwoId = cardsChosenId[1];
 
     if (optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute("src", "../images/blank.png");
-      cards[optionTwoId].setAttribute("src", "../images/blank.png");
+      cards[optionOneId].setAttribute("src", "/blank.png");
+      cards[optionTwoId].setAttribute("src", "/blank.png");
       alert("You have clicked the same image!");
     } else if (cardsChosen[0] === cardsChosen[1]) {
       alert("You found a match");
-      cards[optionOneId].setAttribute("src", "../images/white.png");
-      cards[optionTwoId].setAttribute("src", "../images/white.png");
+      cards[optionOneId].setAttribute("src", "/white.png");
+      cards[optionTwoId].setAttribute("src", "/white.png");
       cards[optionOneId].removeEventListener("click", flipCard);
       cards[optionTwoId].removeEventListener("click", flipCard);
       cardsWon.push(cardsChosen);
     } else {
-      cards[optionOneId].setAttribute("src", "../images/blank.png");
-      cards[optionTwoId].setAttribute("src", "../images/blank.png");
+      cards[optionOneId].setAttribute("src", "/blank.png");
+      cards[optionTwoId].setAttribute("src", "/blank.png");
       alert("Sorry, try again");
     }
     cardsChosen = [];
