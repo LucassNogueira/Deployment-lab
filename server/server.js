4915de397ec44aa194f6729c63f4d3905d62dc94
LucassNogueira <simplylucasjason@gmail.com>
2022-02-10 15:36:49 -0600

    testing endpoints

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const Rollbar = require("rollbar");
-
+const e = require("express");
+const base = "https://deployment-lab020922.herokuapp.com/";
 const app = express();
 
 const rollbar = new Rollbar({
@@ -15,19 +16,22 @@ const rollbar = new Rollbar({
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
+rollbar.log("Howdy Test");
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("https://deployment-lab020922.herokuapp.com/test", (req, res) => {
+  console.log("hit");
   try {
-    nonExistentFunction();
-  } catch (error) {
-    rollbar.error(error);
+    doSomething();
+  } catch (e) {
+    rollbar.error("Something went wrong", e);
+    return;
   }
+  res.status(404).send(res);
 });
-
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log("listening on port " + port));
